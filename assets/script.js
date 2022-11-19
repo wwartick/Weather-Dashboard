50e1398e4850ed142a98d00ab5d379f46f2661f8
wwartick <willwartick@gmail.com>
2022-11-19 14:39:53 -0800

    retrieve city lat/lon to get weather data

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var userFormEl = document.querySelector("#user-form"); 
 var cityInputEl = document.querySelector("#city");
 
+// gets the city data from API
 var getCityData = function(city) {
     var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + "922574b9d5d83d1199eae45856020dd5";
 
@@ -15,11 +16,61 @@ var getCityData = function(city) {
             alert("Error with openweathermap")
         }
     });
-}
+};
 
+// gets the latitude and longtitude to input into the weather API
 var getCityWeather = function(data,city) {
+    if (!data[0]) {
+        alert("Weather data for this city cannot be found");
+    }   
+    else {
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+    }
+    // api call
+    var newApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + "9686278b56b94a147e1e2facc0a2671a";
+
+    fetch(newApiUrl)
+    .then(function(res) {
+        if (res.ok) {
+            res.json().then(function(data) {
+                addToHistory(city); //adds to search history
+                displayCurrentCity(data,city); //displays searched city data
+                displayForecast(data);      //displays forecast for city
+            });
+        } 
+        else {
+            alert("Cannot find weather data");
+        }
+    })
+    .catch(function(err) {
+        console.log("Unable to connect to API");
+    });
+};
+
+var addToHistory = function(city) {
+
+    if (city) {
+
+        if (localStorage.getItem("cities")) {
+            var cityHistoryList = localStorage.getItem("cities");
+        }
+        else {
+            var cityHistoryList = [];
+        }
+
+        var newSearch = city;
+
+        if (cityHistoryList.includes(newSearch)) {
+            console.log(newSearch + " had already been searched");
+        }
+        else {
+            localStorage.setItem("cities", [...[cityHistoryList], newSearch]);
+        }
+    }
 
 }
+
 
 var formSubmitHandler = function(event){
     event.preventDefault();
