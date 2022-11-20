d500bd49806c9bab9a45ffbbe30deab92df7f619
wwartick <willwartick@gmail.com>
2022-11-19 16:50:19 -0800

    added forecasts and cards

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var searchHistoryEl = document.querySelector("#search-history");
 var weatherDataEl = document.querySelector("#weather-data");
+var clearSearchBtn = document.querySelector("#clear-search");
 
 // gets the city data from API
 var getCityData = function(city) {
@@ -38,7 +39,7 @@ var getCityWeather = function(data,city) {
             res.json().then(function(data) {
                 addToHistory(city); //adds to search history
                 displayCurrentCity(data,city); //displays searched city data
-               // displayForecast(data);      //displays forecast for city
+                displayForecast(data);      //displays forecast for city
             });
         } 
         else {
@@ -70,6 +71,7 @@ var addToHistory = function(city) {
             localStorage.setItem("cities", [...[cityHistoryList], newSearch]);
         }
     }
+
 }
 
 var searchAgain = function(event){
@@ -93,8 +95,9 @@ var displaySearchHistory = function() {
             searchContainer.appendChild(citySearchEl);
             citySearchEl.addEventListener("click", searchAgain);
         };
+        searchHistoryEl.appendChild(searchContainer);
     }
-    searchHistoryEl.appendChild(searchContainer);
+
 }
 
 var displayCurrentCity = function (data,city) {
@@ -111,20 +114,24 @@ var displayCurrentCity = function (data,city) {
     var currentCityDivEl = document.createElement("div");
      currentCityDivEl.classList=  "pl-0";
 
+     //displays the city name
     var currentCityH2El = document.createElement("h2");
-     currentCityH2El.classList =  'mt-1 font-weight-bold'
+    currentCityH2El.classList =  'mt-1 font-weight-bold'
     cityDisplayEl = document.createElement("span");
     cityDisplayEl.textContent = city;
 
+    //displays todays date
     var dateDisplayEl = document.createElement("span");
     var date = new Date(data.current.dt * 1000);
     var dateDisplay = date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
     dateDisplayEl.textContent = " " + dateDisplay + " ";
 
+    //icon for weather
     var iconDisplayEl = document.createElement("img");
     iconDisplayEl.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png");
     iconDisplayEl.setAttribute("alt", data.current.weather[0].description + " icon");
 
+    //requested information
     var tempDisplayEl = document.createElement("div");
     tempDisplayEl.innerHTML = "Current temperature: " + data.current.temp + " &#176;F";
 
@@ -134,7 +141,7 @@ var displayCurrentCity = function (data,city) {
     var humidityDisplayEl = document.createElement("div");
     humidityDisplayEl.innerHTML = "Humidity: " + data.current.humidity + " %";
 
-   
+   //appending to html
     weatherDataEl.appendChild(currentCityDivEl);
 
     currentCityDivEl.appendChild(currentCityH2El);
@@ -148,6 +155,53 @@ var displayCurrentCity = function (data,city) {
     currentCityDivEl.appendChild(humidityDisplayEl);
    
 };
+
+var displayForecast = function(data) {
+    
+    var forecastHeaderEl = document.createElement("h4");
+    forecastHeaderEl.innerText = "5-day forecast";
+    weatherDataEl.appendChild(forecastHeaderEl);
+
+    var rowEl = document.createElement("div");
+    rowEl.classList = "row mt-3";
+    weatherDataEl.appendChild(rowEl);
+
+    for (var i=0; i<5; i++) {
+
+        var cardEl = document.createElement("div");
+        cardEl.classList = "card text-bg-secondary";
+        cardEl.style= "width: 12rem";
+
+        var cardBodyEl = document.createElement("div");
+        cardBodyEl.classList = "card-body text-bg-secondary";
+
+        var cardDateEl = document.createElement("h5");
+        var date = new Date(data.daily[i+1].dt * 1000);
+        var dateDisplay = date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+        cardDateEl.innerText = dateDisplay;
+
+        var cardIconEl = document.createElement("img");
+        cardIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i + 1].weather[0].icon + ".png");
+
+        var cardTempEl = document.createElement("div");
+        cardTempEl.innerHTML = "Current temperature: " + data.daily[i+1].temp.day + " &#176;F";
+
+        var cardWindEl = document.createElement("div");
+        cardWindEl.innerHTML = "Wind: " + data.daily[i+1].wind_speed + " mph";
+    
+        var cardHumidityEl = document.createElement("div");
+        cardHumidityEl.innerHTML = "Humidity: " + data.daily[i+1].humidity + " %";
+
+        rowEl.appendChild(cardEl);
+        cardEl.appendChild(cardBodyEl);
+        cardBodyEl.appendChild(cardDateEl);
+        cardBodyEl.appendChild(cardIconEl);
+        cardBodyEl.appendChild(cardTempEl);
+        cardBodyEl.appendChild(cardWindEl);
+        cardBodyEl.appendChild(cardHumidityEl);
+    };
+    displaySearchHistory();
+}
 
 
 
@@ -165,6 +219,12 @@ var formSubmitHandler = function(event){
     }
 };
 
+var clearHistory = function() {
+    localStorage.clear();
+    location.reload();
+};
+
 displaySearchHistory();
 /*Event Handlers (starters) */
 userFormEl.addEventListener("submit", formSubmitHandler);
+clearSearchBtn.addEventListener("click", clearHistory);
